4a08b1f8279f611cb556e8e09e0e01d08cf1e3dd
VerMr12 <mver3894@gmail.com>
2024-03-09 22:00:31 +0300

    Второй коммит. Основа бота

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('main')
+import telebot
+# токен для бота
+token = '7031939959:AAFOYF-nB-3_37yvr0Q7cXT1NnAf3MW-yvw'
+# обьект бот
+bot = telebot.TeleBot(token)
+
+# приветствие
+@bot.message_handler(commands=['start'])
+    # функция ответа
+def welcome_message(message):
+    welcome_text = 'Здравствуйте, этот бот - ваш персональный фитнесс ассистент, он сможет помочь вам в подборе тренировочного плана и упражнений'
+    # отправить сообщение
+    bot.send_message(message.chat.id, welcome_text)
+
+@bot.message_handler(commands=['help'])
+    # даем справку о боте
+def information_message(message):
+    bot_information_text = 'Здравствуйте, этот бот - ваш персональный фитнесс ассистент, он сможет помочь вам в подборе тренировочного плана и упражнений.\n\nЭтот бот создат в качестве итогового проекта, является всего лишь помощником и не гарантирует 100-процентного результата.'
+    # отправляем справку
+    bot.send_message(message.chat.id, bot_information_text)
+
+
+
+
+# циклирование работы бота
+bot.polling()
